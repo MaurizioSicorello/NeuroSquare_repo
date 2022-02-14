2d92361d0d346203cc674501b7a9e239ac393fad
MaurizioSicorello <51700288+MaurizioSicorello@users.noreply.github.com>
2022-02-14 12:38:16 +0100

    updateMultiverseResults

--- MVPA_loop.m
+++ MVPA_loop.m
@@ -7,7 +7,7 @@
 numWorkers = 3; % for parallel computing
 kfoldsOuter = 5;
 repeats = 2;
-onlyUseTrainData = false;
+onlyUseTrainData = true;
 
 % settings for testing the loop
 testMode = false;
@@ -42,19 +42,15 @@ cd('Data')
 AHAB2_quest = readtable('AHAB2_psychVars_deidentified');
 PIP_quest = readtable('PIP_psychVars_deidentified');
 
-% AHAB2_vars = AHAB2_quest(:,{'id', 'ER_LookDiff', 'pnsx_pa', 'pnsx_na', 'STAI', 'BDI_TOT', 'NEON', 'NEON1', 'NEON2', 'NEON3', 'NEON4', 'NEON5', 'NEON6', 'NEONX'});
-% PIP_vars = PIP_quest(:,{'id', 'ER_LookDiff', 'PA_rescale', 'NA_rescale', 'Trait_Anxiety', 'BDI_total', 'neoN', 'neoN1', 'neoN2', 'neoN3', 'neoN4', 'neoN5', 'neoN6', 'NEONX_empty'});
-% 
-% All_Y = array2table([AHAB2_vars{:,:}; PIP_vars{:,:}], 'VariableNames', ...
-%    {'id', 'ER_LookDiff', 'PA', 'NA', 'STAI', 'BDI', 'neoN', 'neoN1', 'neoN2', 'neoN3', 'neoN4', 'neoN5', 'neoN6', 'NEONX'});
-
-AHAB2_vars = AHAB2_quest(:,{'id', 'pnsx_na', 'STAI', 'BDI_TOT', 'NEON', 'NEON1', 'NEON2', 'NEON3', 'NEON4', 'NEON5', 'NEON6', 'NEONX'});
-PIP_vars = PIP_quest(:,{'id', 'NA_rescale', 'Trait_Anxiety', 'BDI_total', 'neoN', 'neoN1', 'neoN2', 'neoN3', 'neoN4', 'neoN5', 'neoN6', 'NEONX_empty'});
+AHAB2_vars = AHAB2_quest(:,{'id', 'ER_LookDiff', 'pnsx_pa', 'pnsx_na', 'STAI', 'BDI_TOT', 'NEON', 'NEON1', 'NEON2', 'NEON3', 'NEON4', 'NEON5', 'NEON6', 'NEONX'});
+PIP_vars = PIP_quest(:,{'id', 'ER_LookDiff', 'PA_rescale', 'NA_rescale', 'Trait_Anxiety', 'BDI_total', 'neoN', 'neoN1', 'neoN2', 'neoN3', 'neoN4', 'neoN5', 'neoN6', 'NEONX_empty'});
 
 All_Y = array2table([AHAB2_vars{:,:}; PIP_vars{:,:}], 'VariableNames', ...
-   {'id', 'NA', 'STAI', 'BDI', 'neoN', 'neoN1', 'neoN2', 'neoN3', 'neoN4', 'neoN5', 'neoN6', 'NEONX'});
+   {'id', 'ER_LookDiff', 'PA', 'NA', 'STAI', 'BDI', 'neoN', 'neoN1', 'neoN2', 'neoN3', 'neoN4', 'neoN5', 'neoN6', 'NEONX'});
 
 
+%%%%% NOTE: not all model ouputs exist for neoN2_train. Do it after all
+%%%%% full data models are conducted!
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -182,19 +178,7 @@ for i=2:size(All_Y, 2)
                     if testMode == true
                         subject_id = [1:testMode_N]';
                     else
-                        [~,~,subject_id] = unique(fMRI_all_scaled.metadata_table.subject_id,'stable')
-                    end
-                    
-
-                    if ML_algorithm(m) == 'oob_rf'
-                        [r, fullOutputs] = cv_ranfor_repeat(fMRI_all_scaled, kfoldsOuter, repeats, 1000, numWorkers, true, subject_id)
-                        
-                    elseif ML_algorithm(m) == 'cv_svm'
-                        [r, fullOutputs] = cv_svm_repeat(fMRI_all_scaled, kfoldsOuter, repeats, subject_id)
-                        
-                    else
-                        maxComps = floor(length(subject_id)*((kfoldsOuter-1)/kfoldsOuter)^2)-1;
-                        [r, fullOutputs] = nestedCrossValRepeat(fMRI_all_scaled, char(ML_algorithm(m)), 1, maxComps, 'integer', kfoldsOuter, repeats, subject_id, 4);     
+                        [~,~,subject_id] = unique(fMRI_all_scaled.metadata_table.subject_id,'stable');
                     end
                     
                     % output label of model
@@ -207,8 +191,30 @@ for i=2:size(All_Y, 2)
                             ML_algorithm(m), '.mat');
                     outLabel = char(outLabel);
                     
+                    % calculate model if it doesn't already exist
+                    disp(['CALCULATING THE FOLLOWING MODEL: ', outLabel])
                     cd('../../Results/Multiverse_Loop')
-                    save(outLabel, 'r', 'fullOutputs')
+                    if isfile(outLabel) ~= 1
+                        
+                        if ML_algorithm(m) == 'oob_rf'
+                        [r, fullOutputs] = cv_ranfor_repeat(fMRI_all_scaled, kfoldsOuter, repeats, 1000, numWorkers, true, subject_id)
+                        
+                        elseif ML_algorithm(m) == 'cv_svm'
+                        [r, fullOutputs] = cv_svm_repeat(fMRI_all_scaled, kfoldsOuter, repeats, subject_id)
+                        
+                        else
+                        maxComps = floor(length(subject_id)*((kfoldsOuter-1)/kfoldsOuter)^2)-1;
+                        [r, fullOutputs] = nestedCrossValRepeat(fMRI_all_scaled, char(ML_algorithm(m)), 1, maxComps-1, 'integer', kfoldsOuter, repeats, subject_id, 4);     
+                        end
+                        
+                        % save model
+                        save(outLabel, 'r', 'fullOutputs')
+                        
+                    else
+                        disp('MODEL ALREADY EXISTS IN RESULTS FOLDER. SKIPPING TO NEXT MODEL')
+                        
+                    end
+                   
                     cd('../../Data/Subject-level-maps')
                     
                 end
